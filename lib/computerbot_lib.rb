454e18d8146a13b4f6ca648f475056ebe26cb263
Darius <jdarius@jdarius.local>
2008-12-20 16:02:39 -0500

    added a case statement

--- lib/computerbot_lib.rb
+++ lib/computerbot_lib.rb
@@ -364,22 +364,16 @@ class Helper
   
   def self.didwhen(old_time) # stolen from http://snippets.dzone.com/posts/show/5715
     val = Time.now - old_time
-     if val < 10 then
-       result = 'just a moment ago'
-     elsif val < 40  then
-       result = 'less than ' + (val * 1.5).to_i.to_s.slice(0,1) + '0 seconds ago'
-     elsif val < 60 then
-       result = 'less than a minute ago'
-     elsif val < 60 * 1.3  then
-       result = "1 minute ago"
-     elsif val < 60 * 50  then
-       result = "#{(val / 60).to_i} minutes ago"
-     elsif val < 60  * 60  * 1.4 then
-       result = 'about 1 hour ago'
-     elsif val < 60  * 60 * (24 / 1.02) then
-       result = "about #{(val / 60 / 60 * 1.02).to_i} hours ago"
-     else
-       result = old_time.strftime("%H:%M %p %B %d, %Y")
+    result = case val
+      when 0..10;               'just a moment ago'
+      when 0..40;               'less than ' + (val * 1.5).to_i.to_s.slice(0,1) + '0 seconds ago'
+      when 0..60;               'less than a minute ago'
+      when 0..(60 * 1.3);       "1 minute ago"
+      when 0..(60 * 50);        "#{(val / 60).to_i} minutes ago"
+      when 0..(60  * 60  * 1.4); 'about 1 hour ago'
+      when 0..(60  * 60 * (24 / 1.02)); "about #{(val / 60 / 60 * 1.02).to_i} hours ago"
+      else
+        old_time.strftime("%H:%M %p %B %d, %Y")
      end
     return "#{result}"
   end # self.didwhen
